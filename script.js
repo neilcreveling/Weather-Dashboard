2a931ddf7de3cc341022c38327607711050fb513
Neil Creveling <neil.creveling@gmail.com>
2021-03-12 16:26:18 -0500

    create variables in js

---
+++ script.js
@@ -0,0 +1,31 @@
+// create variables
+const bgNight = document.querySelector('.night');
+const bgDay = document.querySelector('.day');
+const bgSunset = document.querySelector('.sunset');
+
+const searchForm = document.querySelector('form');
+const searchIcon = document.querySelector('#searchIcon');
+const locationInput = document.querySelector('input');
+const degContainer = document.querySelector('.deg-cont');
+const degToggle = document.querySelector('#degToggle');
+const toggleF = document.querySelector('#toggleF');
+const toggleC = document.querySelector('#toggleC');
+const degMeasurement = document.querySelector('#degMeasurement');
+const currentTemp = document.querySelector('.currentTemp');
+const currentFeels = document.querySelector('.currentFeels');
+const currentHumidity = document.querySelector('.currentHumidity');
+const uvIndex = document.querySelector('.currentUV');
+const uvIcon = document.querySelector('#uvIcon');
+const uvKey = document.querySelector('#uvKey');
+const currentDesc = document.querySelector('.currentDesc');
+const currentWind = document.querySelector('.currentWind');
+const currentIcon = document.querySelector('.currentIcon');
+const sectionHourly = document.querySelector('.hourly-card');
+const sectionDaily = document.querySelector('.daily');
+
+let lat;
+let long;
+let unit = degMeasurement.dataset.unit;
+
+const date = document.querySelector('#date');
+const time = document.querySelector('#time');
